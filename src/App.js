c545fdb6da7c5a35978ccbe9ae79012460f0b8c8
HengistZ <1828195480@qq.com>
2023-04-28 11:41:08 +0800

    git test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hengist Medicine Manager</h2>
+        <h3>git test</h3>
         {/*<p>*/}
         {/*  Edit <code>src/App.js</code> and save to reload.*/}
         {/*</p>*/}
